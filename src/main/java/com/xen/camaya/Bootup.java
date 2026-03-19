5893b3675d90ec642acfed3b03ac7a61f7ca4a1f
XenDQ <xeanxyrone@gmail.com>
2026-03-19 19:10:04 +0800

    feat: added psvm springboot app to main class

--- src/main/java/com/xen/camaya/Bootup.java
+++ src/main/java/com/xen/camaya/Bootup.java
@@ -1,5 +1,11 @@
 package com.xen.camaya;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Bootup {
+    public static void main(String[] args) {
+        SpringApplication.run(Bootup.class, args);
+    }
 }
