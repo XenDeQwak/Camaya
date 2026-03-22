e5fa5e334a668fbb19e1027967b9854dcc09499f
XenDQ <xeanxyrone@gmail.com>
2026-03-22 21:56:47 +0800

    feat: changed requestmapping of login

--- src/main/java/com/xen/camaya/controller/LoginController.java
+++ src/main/java/com/xen/camaya/controller/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class LoginController {
 
